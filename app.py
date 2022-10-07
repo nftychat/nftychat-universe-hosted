d099ae49c7510446ea479c763b28503d3a2ac8fa
Darian Bailey <darian@nftychat.xyz>
2022-10-07 13:24:29 -0400

    init

---
+++ app.py
@@ -0,0 +1,55 @@
+from os.path import isfile
+from fastapi import Response, FastAPI, Request  
+from mimetypes import guess_type
+from fastapi.staticfiles import StaticFiles
+import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory="build")
+
+app = FastAPI()
+# Fronted Mounting (Prod/staging fail without them)
+app.mount("/static", StaticFiles(directory="./build/static"), name="static")
+app.mount("/public", StaticFiles(directory="./build"), name="public")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/universal_dm.js")
+async def get_universal_dm_js():
+    filename = "./build/static/js/main.js"
+            
+    if not isfile(filename):
+        return Response(status_code=404)
+
+    with open(filename) as f:
+        content = f.read()
+
+    content_type, _ = guess_type(filename)
+    return Response(content, media_type=content_type)
+
+@app.get("/universal_dm.css")
+async def get_universal_dm_css():
+    filename = "./build/static/css/main.css"
+
+    if not isfile(filename):
+        return Response(status_code=404)
+
+    with open(filename) as f:
+        content = f.read()
+
+    content_type, _ = guess_type(filename)
+    return Response(content, media_type=content_type)
+
+# Handles serving of app
+@app.api_route("/{path_name:path}", methods=["GET"], include_in_schema=False)
+def serve_app(request: Request, response: Response):
+    response.headers["Cache-Control"] = "s-maxage=3600"
+    return templates.TemplateResponse("index.html", {"request": request})
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=8081)
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {UniversalDm} from 'nftychat-universe'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const div = document.getElementById("nfty_universal_dm");
+const root = ReactDOM.createRoot(div);
 root.render(
   <React.StrictMode>
-    <App />
+    <UniversalDm
+      address={div.dataset.address}
+      displayName={div.dataset.display_name}
+      theme={div.dataset.theme}
+    />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
