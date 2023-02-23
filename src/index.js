6970a38835c1c0501dd6921de59cb1b8ac32e152
Darian Bailey <darian@nftychat.xyz>
2023-02-22 22:19:46 -0500

    fix the example

--- src/index.js
+++ src/index.js
@@ -1,29 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {UniversalDm, UniversalSupport} from 'nftychat-universe'
+import { UniversalDm, UniversalSupport } from "nftychat-universe";
 
 const dm_div = document.getElementById("nfty_universal_dm");
-const dm_root = ReactDOM.createRoot(dm_div);
-dm_root.render(
-  <React.StrictMode>
-    <UniversalDm
-      address={dm_div.dataset.address}
-      displayName={dm_div.dataset.display_name}
-      theme={dm_div.dataset.theme}
-    />
-  </React.StrictMode>
-);
-
+if (!dm_div) {
+  const dm_root = ReactDOM.createRoot(dm_div);
+  dm_root.render(
+    <React.StrictMode>
+      <UniversalDm
+        address={dm_div.dataset.address}
+        displayName={dm_div.dataset.display_name}
+        theme={dm_div.dataset.theme}
+      />
+    </React.StrictMode>
+  );
+}
 
 const support_div = document.getElementById("nfty_universal_support");
-const support_root = ReactDOM.createRoot(support_div);
-support_root.render(
-  <React.StrictMode>
-    <UniversalSupport
-      address={support_div.dataset.address}
-      chatTitle={support_div.dataset.chat_title}
-      welcomeMessage={support_div.dataset.chat_title}
-      theme={support_div.dataset.theme}
-    />
-  </React.StrictMode>
-);
+if (support_div) {
+  const support_root = ReactDOM.createRoot(support_div);
+  support_root.render(
+    <React.StrictMode>
+      <UniversalSupport
+        address={support_div.dataset.address}
+        chatTitle={support_div.dataset.chat_title}
+        welcomeMessage={support_div.dataset.welcome_message}
+        theme={support_div.dataset.theme}
+      />
+    </React.StrictMode>
+  );
+}
