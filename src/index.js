d031b78b7b82d13b4e88222157b11246acdd42e4
Darian Bailey <darian@nftychat.xyz>
2023-02-22 21:39:26 -0500

    add support

--- src/index.js
+++ src/index.js
@@ -2,14 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import {UniversalDm} from 'nftychat-universe'
 
-const div = document.getElementById("nfty_universal_dm");
-const root = ReactDOM.createRoot(div);
-root.render(
+const dm_div = document.getElementById("nfty_universal_dm");
+const dm_root = ReactDOM.createRoot(div);
+dm_root.render(
   <React.StrictMode>
     <UniversalDm
-      address={div.dataset.address}
-      displayName={div.dataset.display_name}
-      theme={div.dataset.theme}
+      address={dm_div.dataset.address}
+      displayName={dm_div.dataset.display_name}
+      theme={dm_div.dataset.theme}
+    />
+  </React.StrictMode>
+);
+
+
+const support_div = document.getElementById("nfty_universal_support");
+const support_root = ReactDOM.createRoot(div);
+support_root.render(
+  <React.StrictMode>
+    <UniversalSupport
+      address={support_div.dataset.address}
+      chatTitle={support_div.dataset.chat_title}
+      welcomeMessage={support_div.dataset.chat_title}
+      theme={support_div.dataset.theme}
     />
   </React.StrictMode>
 );
